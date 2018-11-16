0a69ac0218fbb5fefc8d4731f695297dcdf59e9b
tk304 <tk304@pvsd.org>
2018-11-16 11:44:57 -0500

    red family man

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 import com.sun.xml.internal.ws.policy.PolicyMapUtil;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 
 import java.io.*;
 import java.util.*;
@@ -13,6 +14,20 @@ public class Main {
         } catch (NumberFormatException e) {
             return false;
         }
+    }
+
+    void bubbleSort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (arr[j] > arr[j+1])
+                {
+                    // swap temp and arr[i]
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
     }
 
     public static void main(String[] args) throws IOException {
---
+++ src/com/company/School.java
@@ -0,0 +1,16 @@
+package com.company;
+
+/**
+ * Created by tk304 on 11/14/18.
+ */
+public class School {
+
+    public String schoolName;
+    public int averageScore;
+
+    public School(String name, int averageSATScore)
+    {
+        this.schoolName = name;
+        this.averageScore = averageSATScore;
+    }
+}
